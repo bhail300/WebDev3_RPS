3eedcafdb3390d3ebbcdd5faafb4e8abfb795cb5
SeanHLam <91505213+SeanHLam@users.noreply.github.com>
2022-11-01 20:02:05 -0700

    added text

--- rps/pages/index.js
+++ rps/pages/index.js
@@ -3,7 +3,40 @@ import {ButtonCont, SelectionButton} from './styles'
 import Form from './components/forms/forms'
 import { Wrapper } from './styles'
 import { BackgroundImage } from './styles'
-export default function Home() {
+import AppText from './components/text/text'
+import React, { useState } from 'react';
+
+export default function Home()  {
+  var [player, setPlayer] = useState("none");
+  var [cpu, setCpu] = useState("none");
+  var [streak, setStreak] = useState(0);
+  
+  const cpuChoice = () =>{
+    var choices = ["rock", 'paper', 'scissors']
+    var numchoice = Math.floor(Math.random() * 3)
+    return(choices[numchoice])
+  }
+
+  const handleChoice = (choice) =>{
+    setPlayer(choice)
+    setCpu(cpuChoice())
+    var score = 0
+
+    if(player === "rock" && cpu === "scissors"){
+      setStreak(streak += 1)
+    } else if(player === "scissors" && cpu === "paper"){
+      setStreak(streak += 1)
+    } else if(player === "paper" && cpu === "rock"){
+      setStreak(streak += 1)
+    } else if(player === "rock" && cpu === "paper"){
+      setStreak(0)
+    } else if(player === "paper" && cpu === "scissors"){
+      setStreak(0)
+    } else if(player === "scissors" && cpu === "rock"){
+      setStreak(0)
+    }
+  }
+  
   return (
     <div>
       <Form></Form>
@@ -12,6 +45,16 @@ export default function Home() {
       <SelectionButton>Paper</SelectionButton>
       <SelectionButton>Scissors</SelectionButton>
       </ButtonCont>
+
+      <AppText></AppText>
+       
+       <div>Win Streak: {streak}</div>
+       <div>The Player Chose: {player}</div>
+       <div>The Cpu Chose: {cpu}</div>
+       <button onClick={()=>handleChoice("rock")}>Rock</button>
+       <button onClick={()=>handleChoice("paper")}>Paper</button>
+       <button onClick={()=>handleChoice("scissors")}>Scissors</button>
+
     </div>
   )
 }
