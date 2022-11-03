cdf6f3f7e78d3df35aa76ddc241c86f0d2ee565f
DesmandD3 <dking68@my.bcit.ca>
2022-11-02 21:04:06 -0700

    added second hand

--- rps/pages/components/handImage/handImage.js
+++ rps/pages/components/handImage/handImage.js
@@ -2,17 +2,24 @@ import React from 'react';
 import styled from 'styled-components';
 
 const HandContainer = styled.div`
-
+    
 `
-const HandImg = styled.img`
-
+const P1HandImg = styled.img`
+height: 50%;
+width: 50%;
+`
+const P2HandImg = styled.img`
+height: 50%;
+width: 50%;
+transform: scaleX(-1);
 `
 
 export default function HandImage() {
     return (
         <HandContainer>
 
-            <HandImg src={'paperHand.png'} alt="hand gesture" />
+            <P1HandImg src={'paperHand.png'} alt="hand gesture" />
+            <P2HandImg src={'rockHand.png'} alt="hand gesture" />
 
         </HandContainer>
     );
