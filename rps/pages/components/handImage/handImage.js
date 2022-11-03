f2335681ca438dcfde07d4e47eaf0684498a7a19
DesmandD3 <dking68@my.bcit.ca>
2022-11-02 19:47:21 -0700

    resolved background image

--- rps/pages/components/handImage/handImage.js
+++ rps/pages/components/handImage/handImage.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Wrapper } from '../../styles';
-// import paperH from '../../../images/paperHand.png';
 
 const HandContainer = styled.div`
 
@@ -12,8 +10,10 @@ const HandImg = styled.img`
 
 export default function HandImage() {
     return (
-        <div>
-            <HandImg src={'https://cdn.discordapp.com/attachments/1031694947902038100/1037384145237717002/paperHand.png'} alt="hand gesture"/>
-        </div>
+        <HandContainer>
+
+            <HandImg src={'paperHand.png'} alt="hand gesture" />
+
+        </HandContainer>
     );
 }
